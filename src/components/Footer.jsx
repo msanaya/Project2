227e80aadbcf4ee25282d0cb9085cae93dbee30e
msanaya <50601483+msanaya@users.noreply.github.com>
2020-09-11 12:23:27 -0400

    Fixed emojis stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "../styling/Footer.css";
 const Footer = () => (
   <div className="footer">
     <p>"Everything will be alright at the end, and if it's not alright, it's still not the end."</p>
-    <p>Made with love ❤️ by Pilar B. ❤️</p>
+    <p>Made with love <span role="img">❤️</span> by Pilar B. <span role="img">❤️</span></p>
   </div>
 );
 
